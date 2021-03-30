ec56f506aea90d8e4da55c2976702d23d21b52c0
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-03-30 14:05:00 -0500

    completed slides 1

--- src/main/java/com/tts/TechTalentTwitter/service/UserService.java
+++ src/main/java/com/tts/TechTalentTwitter/service/UserService.java
@@ -1,9 +1,16 @@
 package com.tts.TechTalentTwitter.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.tts.TechTalentTwitter.model.Role;
+import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.repository.RoleRepository;
 import com.tts.TechTalentTwitter.repository.UserRepository;
 
@@ -21,5 +28,29 @@ public class UserService {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+        
+    public List<User> findAll(){
+        return (List<User>) userRepository.findAll();
+    }
+        
+    public void save(User user) {
+        userRepository.save(user);
+    }
+    public User saveNewUser(User user) {
+        //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        //user.setActive(1);
+        //Role userRole = roleRepository.findByRole("USER");
+        //user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+    public User getLoggedInUser() {
+        String loggedInUsername = SecurityContextHolder.
+          getContext().getAuthentication().getName();
+        
+        return findByUsername(loggedInUsername);
     }
 }
