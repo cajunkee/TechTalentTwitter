446e1d24f0fce1160468b17a43236494f6d7f30d
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-04-04 12:46:45 -0500

    updates user controller

---
+++ src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.tts.TechTalentTwitter.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.tts.TechTalentTwitter.service.TweetService;
+import com.tts.TechTalentTwitter.service.UserService;
+
+@Controller
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+    
+    @Autowired
+    private TweetService tweetService;
+    
+
+    @GetMapping(value = "/users/{username}")
+    public String getUser(@PathVariable(value="username") String username, Model model) {	
+    User user = userService.findByUsername(username);
+    List<Tweet> tweets = tweetService.findAllByUser(user);
+    }
+    model.addAttribute("tweetList", tweets);
+    model.addAttribute("user", user);
+    return "user";
+    
+    @GetMapping(value = "/users")
+    public String getUsers(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        SetTweetCounts(users, model);
+        return "users";
+    }
+}
