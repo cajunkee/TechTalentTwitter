a981f2c6c9f2006455447de5237677dc713308b2
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-04-04 12:47:51 -0500

    reupdates user controller file

--- src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.tts.TechTalentTwitter.controller;
 
 import java.util.List;
+import java.util.HashMap;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.tts.TechTalentTwitter.model.Tweet;
 import com.tts.TechTalentTwitter.service.TweetService;
 import com.tts.TechTalentTwitter.service.UserService;
 
@@ -25,10 +28,10 @@ public class UserController {
     public String getUser(@PathVariable(value="username") String username, Model model) {	
     User user = userService.findByUsername(username);
     List<Tweet> tweets = tweetService.findAllByUser(user);
-    }
     model.addAttribute("tweetList", tweets);
     model.addAttribute("user", user);
     return "user";
+    }
     
     @GetMapping(value = "/users")
     public String getUsers(Model model) {
@@ -37,4 +40,14 @@ public class UserController {
         SetTweetCounts(users, model);
         return "users";
     }
+    
+    private void SetTweetCounts(List<User> users, Model model) {
+        HashMap<String,Integer> tweetCounts = new HashMap<>();
+       
+        for (User user : users) {
+            List<Tweet> tweets = tweetService.findAllByUser(user);
+            tweetCounts.put(user.getUsername(), tweets.size());
+        }
+        model.addAttribute("tweetCounts", tweetCounts);
+    } 
 }
